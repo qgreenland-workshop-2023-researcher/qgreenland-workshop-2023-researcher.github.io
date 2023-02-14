881484cd6a280fd9023de10d52918f16bd736976
Matt Fisher <matt.fisher@nsidc.org>
2023-02-13 17:24:55 -0700

    Resolve spelling complaint

--- scripts/spell-check.sh
+++ scripts/spell-check.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ "${1-}" = "-i" ]; then
+    # Suggestions are super slow, so we want them disabled in interactive mode.
+    REPORT_MODE="--no-suggestions"
+else
+    REPORT_MODE="--report"
+fi
+
 # Check spelling on markdown files, but ignore some ephemeral files (e.g.
 # Quarto stuff) and files from external sources (e.g. Code of Conduct, node
 # modules)
-npx mdspell --en-us --ignore-numbers --ignore-acronyms --report \
+npx mdspell --en-us --ignore-numbers --ignore-acronyms ${REPORT_MODE} \
     $(find . \
         -not -path './CODE_OF_CONDUCT.md' \
         -not -path './node_modules/*' \
