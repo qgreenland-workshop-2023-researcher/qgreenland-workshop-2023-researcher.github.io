c86bdc21a30756bea58975fdca49c37caf51658c
Trey Stafford <trey.stafford@colorado.edu>
2023-03-30 10:38:57 -0600

    Add `rm` of output file for raster scenario

--- content/exercises/data-scenarios/raster-needs-reformatting/solutions/solution-gdal_translate.sh
+++ content/exercises/data-scenarios/raster-needs-reformatting/solutions/solution-gdal_translate.sh
@@ -5,4 +5,7 @@ INPUT_FILE="${THIS_DIR}/../datafile.jpg"
 OUTPUT_FILE="${THIS_DIR}/datafile.tif"
 PROJECTION_DEFINITION_FILE="${THIS_DIR}/../datafile.prj"
 
+# Remove the output file if it already exists.
+rm -f $OUTPUT_FILE
+
 gdal_translate -a_srs $PROJECTION_DEFINITION_FILE -a_ullr -334334.200 -1855796.425 -327188.537 -1860658.423 $INPUT_FILE $OUTPUT_FILE
