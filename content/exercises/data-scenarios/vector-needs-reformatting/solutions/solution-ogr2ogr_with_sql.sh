c88e3755faabfbe3831203af5d5b1760a73d2792
Trey Stafford <trey.stafford@colorado.edu>
2023-03-30 10:20:25 -0600

    Update `vector-needs-reformatting` solutions

--- content/exercises/data-scenarios/vector-needs-reformatting/solutions/solution-ogr2ogr_with_sql.sh
+++ content/exercises/data-scenarios/vector-needs-reformatting/solutions/solution-ogr2ogr_with_sql.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+INPUT_FILE="${THIS_DIR}/../kcbcc_DS4_final_v2_final.xlsx"
+OUTPUT_FILE="${THIS_DIR}/output.geojson"
+
+# Remove the output file if it already exists.
+rm -f $OUTPUT_FILE
+
 # The data in a row looks like:
 #     {
 #         "x": -49.6612421393137,
@@ -17,5 +24,6 @@ ogr2ogr -f "GeoJSON" \
     -dialect "sqlite" \
     -sql "SELECT ST_GeomFromText('POINT(' || x || ' ' || y || ')', 4326) geom, cuteness_rating, blood_acetone_grams_per_ml, blood_acetylcholine_grams_per_ml from Sheet1" \
     -a_srs "EPSG:4326" \
-    "output.geojson" \
-    "../kcbcc_DS4_final_v2_final.xlsx"
+    -overwrite \
+    $OUTPUT_FILE \
+    $INPUT_FILE
